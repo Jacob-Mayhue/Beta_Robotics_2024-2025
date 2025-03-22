01626f035934cb1b6d74880d4ae5411680ad0e68
jacob mayhue <jacobmayhue@icloud.com>
2025-03-21 22:32:01 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
@@ -8,22 +8,26 @@ import com.qualcomm.robotcore.hardware.Servo;
 import java.util.Arrays;
 import java.util.List;
 
-/*TODO:  In our lesson on Wednesday I showed you how to create a RobotCore class.
-   If you need a refresher, your RobotCore class should house all of the physical objects on your robot
-   such as motors and servos.  Your job is to create a simple RobotCore class that declares and initializes all components
-   of the new Chim-ung robot.  (This will be the robot we use for all training)
- */
+//   In our lesson on Wednesday I showed you how to create a RobotCore class.
+//   If you need a refresher, your RobotCore class should house all of the physical objects on your robot
+//   such as motors and servos.  Your job is to create a simple RobotCore class that declares and initializes all components
+//   of the new Chim-ung robot.  (This will be the robot we use for all training)
 
-/*TODO: complete the RobotCore class in these steps
+
+/*
+TODO: complete the RobotCore class in these steps
     note: I have already imported all the necessary classes, but if problems arise use alt+enter
-    1. Extend the class to OpMode
-    2. Declare all motors and Servos
-    3. Initialize all motors and servos (HINT: objectName = hardwareMap.get(hardwareType.class, "objectName in config");)
+    1. Extend the class to OpMode (after extending the class use alt+enter to add the two required functions to clear the error)
+    2. Declare all motors and Servos outside of both the init and loop functions
+    3. Initialize all motors and servos in the init() function
+    (HINT: objectName = hardwareMap.get(hardwareType.class, "objectName in config");)
     4. Set the ZeroPowerBehavior of all motors to BRAKE (HINT: just type "objectName." and look at the options for auto complete)
     5. Set all motor modes to RUN_USING_ENCODER
     6. (Optional) If you want to challenge your Java knowledge create a list that stores all your motors and set their
-       ZeroPowerBehaviors and Modes using a single for loop*/
-public class RobotCore
-{
+       ZeroPowerBehaviors and Modes using a single for loop
+*/
+public class RobotCore {
+
+
 
 }
