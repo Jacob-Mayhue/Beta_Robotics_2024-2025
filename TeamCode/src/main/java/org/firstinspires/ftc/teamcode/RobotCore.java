f84f093e258cf7a3b09b416107457e8d30c0fe35
jacob mayhue <jacobmayhue@icloud.com>
2025-03-21 22:04:15 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
@@ -8,41 +8,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 import java.util.Arrays;
 import java.util.List;
 
-public class RobotCore extends OpMode
+/*TODO:  In our lesson on Wednesday I showed you how to create a RobotCore class.
+   If you need a refresher, your RobotCore class should house all of the physical objects on your robot
+   such as motors and servos.  Your job is to create a simple RobotCore class that declares and initializes all components
+   of the new Chim-ung robot.  (This will be the robot we use for all training)
+ */
+
+/*TODO: complete the RobotCore class in these steps
+    note: I have already imported all the necessary classes, but if problems arise use alt+enter
+    1. Extend the class to OpMode
+    2. Declare all motors and Servos
+    3. Initialize all motors and servos (HINT: objectName = hardwareMap.get(hardwareType.class, "objectName in config");)
+    4. Set the ZeroPowerBehavior of all motors to BRAKE (HINT: just type "objectName." and look at the options for auto complete)
+    5. Set all motor modes to RUN_USING_ENCODER
+    6. (Optional) If you want to challenge your Java knowledge create a list that stores all your motors and set their
+       ZeroPowerBehaviors and Modes using a single for loop*/
+public class RobotCore
 {
-    DcMotor frontLeft, frontRight, backLeft, backRight, slide;
-    Servo clawPivot, slidePivot;
-    CRServo leftWheel, rightWheel;
 
-    List<DcMotor> motors;
-    @Override
-    public void init() {
-
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        slide = hardwareMap.get(DcMotor.class, "slide");
-
-        clawPivot = hardwareMap.get(Servo.class, "clawPivot");
-        slidePivot = hardwareMap.get(Servo.class, "slidePivot");
-
-        leftWheel = hardwareMap.get(CRServo.class, "leftWheel");
-        rightWheel = hardwareMap.get(CRServo.class, "rightWheel");
-        motors = Arrays.asList(frontLeft, frontRight, backLeft, backRight);
-
-        for(DcMotor motor: motors){
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
-
-    }
-
-    @Override
-    public void loop() {
-
-    }
 }
