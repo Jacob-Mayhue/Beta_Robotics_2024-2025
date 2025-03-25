b16948a95f2d418ef0975db4e94aa5b1dc081f09
jacob mayhue <jacobmayhue@icloud.com>
2025-03-25 11:03:55 -0400

    added BuildConfig class to fix error

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/BuildConfig.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.robotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String APP_BUILD_TIME = "2025-02-28T14:44:20.795-0500";
+}
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -78,7 +78,7 @@ import com.qualcomm.ftccommon.configuration.EditParameters;
 import com.qualcomm.ftccommon.configuration.FtcLoadFileActivity;
 import com.qualcomm.ftccommon.configuration.RobotConfigFile;
 import com.qualcomm.ftccommon.configuration.RobotConfigFileManager;
-import com.qualcomm.ftcrobotcontroller.BuildConfig;
+import org.firstinspires.ftc.robotcontroller.BuildConfig;
 import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.hardware.HardwareFactory;
 import com.qualcomm.robotcore.eventloop.EventLoopManager;
@@ -125,7 +125,6 @@ import org.firstinspires.ftc.robotcore.internal.ui.UILocation;
 import org.firstinspires.ftc.robotcore.internal.webserver.RobotControllerWebInfo;
 import org.firstinspires.ftc.robotserver.internal.programmingmode.ProgrammingModeManager;
 import org.firstinspires.inspection.RcInspectionActivity;
-import org.threeten.bp.YearMonth;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.FileNotFoundException;
@@ -843,3 +842,4 @@ public class FtcRobotControllerActivity extends Activity
     }
   }
 }
+
