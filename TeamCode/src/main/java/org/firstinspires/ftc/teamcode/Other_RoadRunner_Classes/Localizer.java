c2268d33046738c18cf30add7333f7dc6a6cec36
jacob mayhue <jacobmayhue@icloud.com>
2025-04-01 09:51:35 -0400

    added RoadRunner Classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other_RoadRunner_Classes/Localizer.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Other_RoadRunner_Classes;
+
+import com.acmerobotics.roadrunner.Time;
+import com.acmerobotics.roadrunner.Twist2dDual;
+
+public interface Localizer {
+    Twist2dDual<Time> update();
+}
